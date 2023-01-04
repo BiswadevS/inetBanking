99d1838466aa31355a1c30c9a9b48464a90e7cf8
BiswadevS <biswadevsingha99@gmail.com>
2023-01-04 11:39:36 +0530

    This is second commit

--- src/test/java/com/testCases/TC_Edit_Customer.java
+++ src/test/java/com/testCases/TC_Edit_Customer.java
@@ -1,2 +1,7 @@
 //This is a new test case
 //This test will check editing
+
+//This is code added newly
+//This is code added newly
+//This is code added newly
+//This is code added newly
---
+++ src/test/java/com/testCases/TC_Search_Customer.java
@@ -0,0 +1,11 @@
+package com.testCases;
+
+public class TC_Search_Customer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//This is my search customer test case
+	}
+
+}
